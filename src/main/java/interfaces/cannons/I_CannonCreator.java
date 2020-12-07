2758c2f1f2ba8f77b6a7018c3d41afc410690131
TMO <morotmk@gmail.com>
2020-12-07 20:41:29 +0100

    RestApi start

--- src/main/java/interfaces/cannons/I_CannonCreator.java
+++
@@ -1,9 +0,0 @@
-package interfaces.cannons;
-
-import interfaces.cannons.type.E_CannonType;
-
-public interface I_CannonCreator {
-
-    public abstract I_AbstractCannon create (E_CannonType cannonType);
-
-}
--- src/main/java/interfaces/cannons/type/E_CannonType.java
+++
@@ -1,7 +0,0 @@
-package interfaces.cannons.type;
-
-public enum E_CannonType {
-    LASER_CANNON,
-    MACHINE_CANNON,
-    PLASMA_CANNON
-}
